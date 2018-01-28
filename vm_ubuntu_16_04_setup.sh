9ec9eb811ff78eab6b9f785c5e3218aee5e00736
Akhil Bondlela <akhil.bondlela@gmail.com>
2018-01-28 14:12:28 -0500

    Shell script updates

---
+++ vm_ubuntu_16_04_setup.sh
@@ -0,0 +1,70 @@
+# Run updates
+sudo apt-get update
+sudo ap-get upgrade
+
+# Install Pre-reqs
+sudo apt-get install git
+sudo apt-get install cmake
+sudo apt-get install python-pip
+pip install opencv-python
+
+# Installing Pangolin
+git clone https://github.com/stevenlovegrove/Pangolin.git
+sudo apt-get install libglew-dev
+sudo apt-get install libpython2.7-dev
+sudo apt-get install ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev
+sudo apt-get install libdc1394-22-dev libraw1394-dev
+sudo apt-get install libjpeg-dev libpng12-dev libtiff5-dev libopenexr-dev
+cd Pangolin
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cd ~
+
+# Installing OpenCV 3.2.x
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python3.5-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
+sudo -s
+cd ~/opt/
+git clone https://github.com/Itseez/opencv.git
+git clone https://github.com/Itseez/opencv_contrib.git
+cd opencv
+mkdir release
+cd release
+cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
+make -j4
+make install
+ldconfig
+exit
+cd ~
+
+#Installing ORB-SLAM2
+cd ~
+git clone https://github.com/raulmur/ORB_SLAM2.git ORB_SLAM2
+cd ORB_SLAM2
+chmod +x build.sh
+./build.sh
+
+#Installing ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+#Setting up a Catkin workspace
+source /opt/ros/kinetic/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+
+echo "VM CONFIGURED PROPERLY"
