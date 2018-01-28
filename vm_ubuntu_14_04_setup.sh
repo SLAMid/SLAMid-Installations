4be7142b690d929341834068c3a449f603471f59
Akhil Bondlela <akhil.bondlela@gmail.com>
2018-01-28 15:13:04 -0500

    updates to paths

--- vm_ubuntu_14_04_setup.sh
+++ vm_ubuntu_14_04_setup.sh
@@ -29,7 +29,7 @@ sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavform
 sudo apt-get install python3.5-dev python3-numpy libtbb2 libtbb-dev
 sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
 sudo -s
-cd ~/opt/
+cd /opt/
 git clone https://github.com/Itseez/opencv.git
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv
