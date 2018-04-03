0383bb6ed943b2e527dd57809cf804da1c7c8c67
Akhil Bondlela <akhil.bondlela@gmail.com>
2018-04-03 14:29:25 -0400

    ROS Scripts first prototype done

---
+++ ros_scripts_final.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+"""OpenCV feature detectors with ros CompressedImage Topics in python.
+
+This example subscribes to a ros topic containing sensor_msgs 
+CompressedImage. It converts the CompressedImage into a numpy.ndarray, 
+then detects and marks features in that image. It finally displays 
+and publishes the new image - again as CompressedImage topic.
+"""
+__author__ =  'Simon Haller <simon.haller at uibk.ac.at>'
+__version__=  '0.1'
+__license__ = 'BSD'
+# Python libs
+import sys, time
+
+# numpy and scipy
+import numpy as np
+from scipy.ndimage import filters
+
+# OpenCV
+import cv2
+
+# Ros libraries
+import roslib
+import rospy
+
+# Ros Messages
+from sensor_msgs.msg import CompressedImage
+# We do not use cv_bridge it does not support CompressedImage in python
+# from cv_bridge import CvBridge, CvBridgeError
+
+VERBOSE=False
+
+def random_colors(N):
+    np.random.seed(1)
+    colors = [tuple(255 * np.random.rand(3)) for _ in range(N)]
+    return colors
+
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """apply mask to image"""
+    for n, c in enumerate(color):
+        image[:, :, n] = np.where(
+            mask == 1,
+            image[:, :, n] * (1 - alpha) + alpha * c,
+            image[:, :, n]
+        )
+    return image
+
+
+def display_instances(image, boxes, masks, ids, names, scores):
+    """
+        take the image and results and apply the mask, box, and Label
+    """
+    n_instances = boxes.shape[0]
+    colors = random_colors(n_instances)
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i, color in enumerate(colors):
+        if not np.any(boxes[i]):
+            continue
+
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+        score = scores[i] if scores is not None else None
+        caption = '{} {:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+
+        image = apply_mask(image, mask, color)
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(
+            image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+        )
+
+    return image
+
+
+import os
+import sys
+import coco
+import utils
+import model as modellib
+
+ROOT_DIR = os.getcwd()
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+class InferenceConfig(coco.CocoConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+
+
+
+class image_feature:
+
+    def __init__(self):
+        '''Initialize ros publisher, ros subscriber'''
+        # topic where we publish
+        self.image_pub = rospy.Publisher("/output/image_raw/compressed",
+            CompressedImage)
+        # self.bridge = CvBridge()
+
+        # subscribed Topic
+        self.subscriber = rospy.Subscriber("/usb_cam/image_raw/compressed",
+            CompressedImage, self.callback,  queue_size = 1)
+        #if VERBOSE :
+            #print "subscribed to /camera/image/compressed"
+
+
+    def callback(self, ros_data):
+        '''Callback function of subscribed topic. 
+        Here images get converted and features detected'''
+        #if VERBOSE :
+            #print 'received image of type: "%s"' % ros_data.format
+
+        #### direct conversion to CV2 ####
+        np_arr = np.fromstring(ros_data.data, np.uint8)
+        #image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
+        
+        model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+        model.load_weights(COCO_MODEL_PATH, by_name=True)
+        class_names = [
+           'BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+           'bus', 'train', 'truck', 'boat', 'traffic light',
+           'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+           'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+           'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+           'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+           'kite', 'baseball bat', 'baseball glove', 'skateboard',
+           'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+    'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+    'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+    'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+    'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+    'teddy bear', 'hair drier', 'toothbrush'
+]
+
+        
+        results = model.detect([image_np], verbose=0)
+        r = results[0]
+        image_np = display_instances(
+            image_np, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+        #cv2.imshow('frame', frame)
+        
+        #cv2.imshow('cv_img', image_np)
+        #cv2.waitKey(2)
+
+        #### Create CompressedIamge ####
+        msg = CompressedImage()
+        msg.header.stamp = rospy.Time.now()
+        msg.format = "jpeg"
+        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+        # Publish new image
+        self.image_pub.publish(msg)
+        
+        #self.subscriber.unregister()
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    ic = image_feature()
+    rospy.init_node('image_feature', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down ROS Image feature detector module")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
